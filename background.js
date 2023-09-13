e3d565bafb574504abf0a9c92aaceb84f10461e3
Cordelia Snape <snapecordelia@gmail.com>
2023-09-13 18:54:30 +0100

    css changes

--- background.js
+++ background.js
@@ -3,15 +3,15 @@ let time = prompt("What hour is it? (0-23 - For example: 4pm is 16))");
 let liar = ("Liar, liar, pants on fire");
 
 function changeBgColor(color) {
-    document.header.style.backgroundColor = color;
+    document.body.style.backgroundColor = color;
 if (time <= 11) {
-    document.header.style.backgroundColor = "#e0ac8d;"
+    document.body.style.backgroundColor = "#e0ac8d;"
 } else if (time <= 16) { 
-    document.header.style.backgroundColor = "#d69376;"
+    document.body.style.backgroundColor = "#d69376;"
 } else if (time <= 23) {
-    document.header.style.backgroundColor = "#a15e3a;"
+    document.body.style.backgroundColor = "#a15e3a;"
 } else {
-    document.header.style.backgroundColor = "#a1413a;"
+    document.body.style.backgroundColor = "#a1413a;"
 }
 
 function displayInfo () {
