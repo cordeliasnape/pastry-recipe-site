e21d7728aa85e05f60a46841434bd114b66ec8d2
Cordelia Snape <snapecordelia@gmail.com>
2023-09-14 10:00:43 +0100

    making my js work

--- background.js
+++ background.js
@@ -1,18 +1,22 @@
 let time = prompt("What hour is it? (0-23 - For example: 4pm is 16))");
 
-let liar = ("Liar, liar, pants on fire");
+// let liar = ("Liar, liar, pants on fire");
+
+console.log(time)
 
 function changeBgColor(color) {
     document.body.style.backgroundColor = color;
 if (time <= 11) {
-    document.body.style.backgroundColor = "#e0ac8d;"
+    document.body.style.backgroundColor = "#e0ac8d";
 } else if (time <= 16) { 
-    document.body.style.backgroundColor = "#d69376;"
+    document.body.style.backgroundColor = "#d69376";
 } else if (time <= 23) {
-    document.body.style.backgroundColor = "#a15e3a;"
+    document.body.style.backgroundColor = "#a15e3a";
 } else {
-    document.body.style.backgroundColor = "#a1413a;"
+    document.body.style.backgroundColor = "#a1413a";
 }
+
+changeBgColor();
 
 function displayInfo () {
     alert(liar);
