38ab005ccab230a697605768da96f651931f3b95
Cordelia Snape <snapecordelia@gmail.com>
2023-09-15 15:53:41 +0100

    added then cut while loop

--- prompts.js
+++ prompts.js
@@ -1,5 +1,7 @@
 let username = prompt("What's your name?");
-let daytime = prompt("What time is it?\n(For example - 'morning', 'afternoon' or 'evening')");
-let message = "Good " + daytime + ", " + username +".";
+let daytime = prompt(
+  "What time is it?\n(For example - 'morning', 'afternoon' or 'evening')"
+);
+let message = "Good " + daytime + ", " + username + ".";
 
 document.write(message);
